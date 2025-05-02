4148b7d84e4ee4ef12839e953e3549d0cb37565f
MJH99HKU <15801953299@163.com>
2025-05-03 02:06:17 +0800

    Create save_load_game.h

---
+++ save_load_game.h
@@ -0,0 +1,7 @@
+#ifndef COMP2113-Goup-Project_Group51
+#define COMP2113-Goup-Project_Group51
+#include <vector>
+using std::vector;
+void save_file(vector<vector<char>>p);
+bool load_file(int &gs);
+#endif
