8b0ea2a9c96610dfe985d00f31f5a37acb9abd57
MJH99HKU <15801953299@163.com>
2025-05-03 02:05:10 +0800

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,8 @@
+#ifndef COMP2113-Goup-Project_Group51
+#define COMP2113-Goup-Project_Group51
+void Welcome();
+void select_mode(char &gamemode);
+void instruction();
+void tut_func();
+void congratulations();
+#endif
