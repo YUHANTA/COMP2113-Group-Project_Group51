f157dfe67ae9c3a402a4a6f2e6d0bce5c79e7b2c
Yu Hanshiyang <h3628925@academy11.cs.hku.hk>
2025-05-02 19:14:06 +0800

    Add new insrructions

---
+++ main.h
@@ -0,0 +1,31 @@
+// 2113-five in a row group 51
+#ifndef COMP2113-Goup-Project_Group51
+#define COMP2113-Goup-Project_Group51
+#include <vector>
+#include <string>
+using namespace std;
+using std::vector;
+using std::string;
+
+typedef int LENGTH;
+
+#define black_chess "\xe2\x97\x8f"
+#define white_chess "\xe2\x97\x8e"
+
+extern LENGTH column;
+extern LENGTH row;
+
+extern char gamemode;
+extern char player;
+extern char ai;
+extern unsigned step_count;
+extern int game_score;
+
+extern vector<vector<char>> board;
+
+void swap_player(char& p);
+int judge(vector<vector<char>> b, int n);
+void print_board(vector<vector<char>> p);
+int make_move(vector<vector<char>>& p, string move);
+void BoardSize(LENGTH &r, LENGTH &c);
+#endif
