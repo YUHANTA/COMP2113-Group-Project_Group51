faff3e12612cec841b8afb03f6ae062c631c5a94
YUHANTA <u3628925@connect.hku.hk>
2025-05-03 13:19:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "main.h"
-#include "Threeaimode.h"
-#include "menu.h"
+#include "game.h"
 #include "save_load_game.h"
+#include "PVAmode.h"
+#include "menu.h"
 
 using namespace std;
 
-vector<vector<char>> board;
-LENGTH colomn = 13;         //Basis setting
-LENGTH row = 6;             //define a vector board
+
+vector<vector<char>> board;                    //define a 2D vector board to store chess position.
+LENGTH column= 13;
+LENGTH row = 6;
 
 char gamemode = 'x';
 char player = '1';
 char ai = '0';
-int game_score = 0;
 unsigned step_count = 0;
+int game_score = 0;
 
-void build_board(vector<vector<char>>&p){
-     vector<char> board_temp;
-     for (size_t  i = 0; i < column; i++) 
-         board_temp.push_back(' ');
-     for (size_t i = 0; i < row; i++)
-         p.push_back(board_temp);
+void build_board(vector<vector<char>>& p) // define an empty board
+{
+    vector<char> board_temp;
+    for (size_t i = 0; i != column; i++)
+        board_temp.push_back(' ');            //row
+    for (size_t i = 0; i != row; i++)
+        p.push_back(board_temp);              //column
 }
 
 void print_board(vector<vector<char>> p)
 {
+    /* construct two array with maximum size 26, allocate this area to store alphabet
+     * Use the characteristics of ascii code to assign them one by one
+     * after output, delete it to release memory
+    */
+
     char *row_arr = new char[26]();            //MAX = 26
     char *col_arr = new char[26]();            //MAX = 26
 
@@ -62,56 +69,44 @@ void print_board(vector<vector<char>> p)
         cout << endl;
     }
     cout << endl;
+
     //release memory
     delete [] row_arr;
     delete [] col_arr;
+
 }
 
-void BoardSize(LENGTH &r, LENGTH &c){
-     LENGTH a, b;
-     cout << "Enter Board size you want with format XX YY, please make sure both XX and YY are between 5 - 26" << endl;
-     cout << "It's recommented to make XX = YY, but anyway you can use what you like. Just like ths saying 'This Game is Your World!'" << endl;
-     cout << "Hint: XX = row, YY = column" << endl;
-     cin >> a >> b;
-     while ((a < 5 || b > 26) || (b < 5 || b > 26)){
-        cout << "Your input is invalid. Please input again." << endl;
-	cout << "Enter Board size you want with formats XX YY, please make sure  both XX and YY are between 5 - 26" << endl;
-        cout << "It's recommented to make XX = YY, but anyway you can use what you like. Just like ths saying 'This Game is Your World!'" << endl;
-        cout << "Hint: XX = row, YY = column" << endl;
-        cin >> a >> b;
-     }
-     r = a;
-     c = b;
-}
-
-void swap_player(char& p){
-    if (p == '1')
-        p = '2'; // X -> 0
-    else p = '1';// 0 -> X
-}
-
-int make_move(vector<vector<char>>& p, string move){
+int make_move(vector<vector<char>>& p, string move)
+{
     size_t row_location_letter, column_location_letter;
-    if (move.size() == 2){
-       if (toupper(move[0]) >= 65 && toupper(move[0]) <= 65+row && toupper(move[1]) >= 65 && toupper(move[1]) <= 65 + column){
-           row_location_letter = toupper(move[0]) - 65; // change A-Z to real row number;
-           column_location_letter = toupper(move[1]) - 65;//also same with above-Real column number
-       }
-       else return -1; //no change
+    if (move.size() == 2 )           //valid move
+    {
+        if (toupper(move[0]) >= 65 && toupper(move[0]) <= 65+row && toupper(move[1]) >= 65 && toupper(move[1]) <= 65+column)
+        {
+            //covert alphabet to number
+            row_location_letter = toupper(move[0]) - 65;    //row
+            column_location_letter = toupper(move[1]) - 65; //column
+        }
+        else
+            return -1;      // make no change, i.e. PASS
     }
-    else return -1;
-    if (p[row_location_letter][column_location_letter] == ' '){//judge is it a empty place to put this step
+    else
+        return -1;          // make no change, i.e. PASS
+    if (p[row_location_letter][column_location_letter] == ' ')
+    {
         p[row_location_letter][column_location_letter] = player;
-	return 0;
+        return 0;           // make change success
     }
-    else return -1;
+    else
+        return -1;          // make no change, i.e. PASS
 }
+
 int judge(vector<vector<char>> b, int n)
 {
     char c = '1';
-    for (int chess = 1; chess != 3; chess++)
+    for (int chess = 1; chess != 3; chess++) //                     chess = 1 for player 1 ; chess = 2 for player 2
     {
-        for (size_t i = 0; i <= row - 1; i++)
+        for (size_t i = 0; i <= row-1; i++)
         {
             for (size_t j = 0; j <= column - 5; j++)
             {
@@ -119,7 +114,7 @@ int judge(vector<vector<char>> b, int n)
                     return chess;
             }
         }
-        for (size_t i = 0; i <= row - 5; i++)
+        for (size_t i = 0; i <= row-5; i++)
         {
             for (size_t j = 0; j <= column - 1; j++)
             {
@@ -131,7 +126,7 @@ int judge(vector<vector<char>> b, int n)
         {
             for (size_t j = 0; j <= column - 5; j++)
             {
-                if (b[i][j] == c && b[i + 1][j + 1] == c && b[i + 2][j + 2] == c && b[i + 3][j + 3] == c && b[i + 4][j + 4] == c)
+                if (b[i][j] == c && b[i + 1][j + 1] == c && b[i + 2][j + 2] == c && b[i + 3][j + 3] == c && b[i + 4][j + 4] == c) // check diagonal1
                     return chess;
             }
         }
@@ -139,133 +134,159 @@ int judge(vector<vector<char>> b, int n)
         {
             for (size_t j = 4; j <= column - 1; j++)
             {
-                if (b[i][j] == c && b[i + 1][j - 1] == c && b[i + 2][j - 2] == c && b[i + 3][j - 3] == c && b[i + 4][j - 4] == c)
+                if (b[i][j] == c && b[i + 1][j - 1] == c && b[i + 2][j - 2] == c && b[i + 3][j - 3] == c && b[i + 4][j - 4] == c) // check diagonal2
                     return chess;
             }
         }
-        c = '2'; 
+        c = '2'; // check player '2'.
+                                        // 'return 1' indicates player '1' wins while 'return 2' indicates player '2' wins.
     }
-    if (n == row * column)
-        return 3;
+    if (n == row*column)
+        return 3; // the board if full and no win, end in a draw
     else
-        return 0;
+        return 0; //unfinished
 }
 
-int p_v_p(){
+void swap_player(char& p)
+{
+    if (p == '1')
+        p = '2'; // X -> 0
+    else p = '1';// 0 -> X
+}
+
+int player_vs_player(){
     string move;
+    instruction();
     print_board(board);
-    while (judge(board, step_count) == 0){
-          cout << "Player " << player << ",move: ";
-	  cin >> move;
-	  if (make_move(board, move) == 0){
-	     cout << endl;
-	     print_board(board);
-	     step_count++;
-	  }
-	  else{
-	  cout << "Player " << player << "PASS." << endl;
-	  }
-	  swap_player(player);
+    while (judge(board, step_count) == 0)
+    {
+        cout << "Player " << player << ", move: ";
+        cin >> move;
+        if (make_move(board, move) == 0)
+        {
+            cout << endl;
+            print_board(board);
+            step_count++;
+        }
+        else
+            cout << "Player " << player << " PASS." << endl;
+        swap_player(player);
     }
-    switch (judge(board, step_count)){
-           case 1: 
-	      cout << "player 1 win" << endl;
-	   case 2:
-	      cout << "player 2 win" << endl;
-	   case 3:
-	      cout << "draw" << endl;
-	   default:
-	      break;
+    switch (judge(board, step_count))
+    {
+        case 1:
+            cout << "player 1 win" << endl;
+            break;
+        case 2:
+            cout << "player 2 win" << endl;
+            break;
+        case 3:
+            cout << "draw" << endl;
+            break;
+        default:
+            break;
     }
     return 0;
 }
 
+void BoardSize(LENGTH &r, LENGTH &c)
+{
+    LENGTH tempr, tempc;
+    cout << "Enter Board size you want with format XX YY, please make sure both XX and YY are between 5 - 26" << endl;
+    cout << "XX = row, YY = column" << endl;
+    cin >> tempr >> tempc;
+    while ((tempr < 5 || tempr > 26) || (tempc < 5 || tempc > 26)){
+        cout << "Your input is invalid, Please input again" << endl;
+        cout << "Enter Board size you want with format XX YY please make sure both XX and YY are between 5 - 26" <<endl;
+        cout << "XX = row, YY = column" << endl;
+        cin >> tempr >> tempc;
+    }
+    r = tempr;
+    c = tempc;
+}
+
+
+void InitialLoadFunc(bool &tutorial){
+    string yn;
+    if (load_file(game_score))
+        cout << "Players, welcome back, your current score is " << game_score << endl;
+    else{
+        cout << "Found that you have no save (new players), do you need to tutorial? (Y/N): ";
+        cin >> yn;
+        while (yn != "YES" && yn != "Y" && yn != "NO" && yn != "N" && yn != "yes" && yn != "no" && yn != "y" && yn != "n"){
+            cout << "Invalid input, please input again" << endl;
+            cout << "Do you need to tutorial? (Y/N): ";
+            cin >> yn;
+        }
+        if (yn == "YES" || yn == "Y" || yn == "y" || yn == "yes")
+            tutorial = true;
+        else if (yn == "NO" || yn == "N" || yn == "n" || yn == "no")
+            tutorial = false;
+    }
+}
+
 bool start_new_game(){
-     string x;
-     cout << "Your performance is perfect! How about playing another game? (Y/N):";
-     cout << endl << "Pay attention please: You should input Y or N;" << endl;
-     cout << "Now, your choice:";
-     cin >> x;
-     if (x == "Y") return true;
-     else return false;
-     return false;
+    string yn;
+    cout << "Do you want to play again? (Y/N): ";
+    cin >> yn;
+    while (yn != "YES" && yn != "Y" && yn != "NO" && yn != "N" && yn != "yes" && yn != "no" && yn != "y" && yn != "n"){
+        cout << "Invalid input, please input again" << endl;
+        cout << "Do you want to play again? (Y/N): ";
+        cin >> yn;
+    }
+    if (yn == "YES" || yn == "Y" || yn == "y" || yn == "yes")
+        return true;
+    else if (yn == "NO" || yn == "N" || yn == "n" || yn == "no")
+        return false;
+    return false;
 }
 
-void InitialLoadFunc(bool &hongkong){
-     string y;
-     if (load_file(game_score))
-     {   cout << "Players, welcome back,your current score is " << game_score << endl;
-     }
-     else{
-         cout << "You have new players and no save, do you need our tutorial class. Don't worry it will not occupy too much time.(Y/N): ";
-	 cin >> y;
-	 if (y == "Y") hongkong = true;
-	 else if (y == "N") hongkong = false;
-	 else{ 
-	    cout << "Invalid intput and we will begin our tutorial clss automatically.";
-            hongkong = true;
-	 }
-	}
-}
+int main() {
+    bool tutorial = false;
+    Welcome();
 
-int main(){
-    Welcome() //code in menu
-    //******
+    //************* BE CAREFUL!!!!!
     New_game:
-    //*****
+    //************* BE CAREFUL!!!!!
 
-    bool hongkong = false;
-    InitialLoadFunc(hongkong); //bool transfer value
-    if (hongkong){
-       tut_func();
+    InitialLoadFunc(tutorial);
+    if (tutorial){
+        tut_func();
     }
 
     select_mode(gamemode);
-    BoardSize(row,column);
+    BoardSize(row, column);
     build_board(board);
 
-    if (gamemode == "0"){
-       BoardSize(row, column);
-       build_board(board);
-       cout << "You are playing with Player" << endl;
-       cout << endl;
-       instruction();
-       p_v_p()
+    if (gamemode == '0'){
+        cout << "You are playing with PLAYER" << endl << endl;
+        p_v_p();
     }
+
     else if (gamemode == '1'){
-       BoardSize(row, column);
-       build_board(board);
-       cout << "You are playing with Ai - Easy method" << endl;
-       cout << endl;
-       p_v_a1(game_score);
+        cout << "You are playing with AI (EASY)" << endl << endl;
+        p_v_a1(game_score);
     }
+
     else if (gamemode == '2'){
-       BoardSize(row, column);
-       build_board(board);
-       cout << "You are playing with Ai - Difficult method" << endl;
-       cout << "Wish you lucky!" << endl;
-       cout << endl;
-       p_v_a2(game_score);
+        cout << "You are playing with AI (HARD)" << endl << endl;
+        p_v_a2(game_score);
     }
     else{
-       cout << "In this method, you will fight master ai in 15 x 15 standard board" << endl;
-       row = 15;
-       column = 15;
-       build_board(board);
-       cout << "You are playing with Ai - Master  method" << endl;
-       cout << "Don't lose heart anymore. You can win him" << endl;
-       cout << endl;
-       p_v_a3(game_score);
+        cout << "You are playing with AI (HARD)" << endl << endl;
+        p_v_a3(game_score);
     }
-    //Assume that game mode cannont be other intput other than 0/1/2/3.
 
     congratulations();
     swap_player(player);
     save_file(board);
 
+    //************* BE CAREFUL!!!!!
     if (start_new_game()){
-       board.clear();
-       goto New_game;
+        board.clear();          //construct new board to avoid bugs
+        goto New_game;
     }
-     return 0;
+    //************* BE CAREFUL!!!!!
+
+    return 0;
 }
